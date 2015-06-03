079ede8d99ab2e33871c4237ed9e68f2bcaf8425
Rebecca Skinner <traybaby@gmail.com>
2015-06-03 20:20:34 +0800

    Section 4.4.2: Redirect users back to the projects page if they try going to a project that doesn't exist.
    This also includes minor re-laying-out of code, and the use of plugs
    for the first time.

--- web/controllers/project_controller.ex
+++ web/controllers/project_controller.ex
@@ -3,6 +3,7 @@ defmodule Ticketee.ProjectController do
   alias Ticketee.Project
 
   plug :scrub_params, "project" when action in [:create, :update]
+  plug :load_project when action in [:edit, :update, :show, :destroy]
   plug :action
 
   def index(conn, _) do
@@ -19,43 +20,46 @@ defmodule Ticketee.ProjectController do
     changeset = Project.changeset(%Project{}, project_params)
     if changeset.valid? do
       id = Repo.insert(changeset)
-      conn
-      |> put_flash(:info, "Project created successfully.")
-      |> redirect to: project_path(conn, :show, id)
+      conn  |> put_flash(:info, "Project created successfully.")
+            |> redirect to: project_path(conn, :show, id)
     else
       render conn, :new, changeset: changeset
     end
   end
 
-  def show(conn, %{"id" => id}) do
-    project = Repo.get(Project, id)
-    render conn, :show, project: project
+  def show(conn, _) do
+    render conn, :show
   end
 
   def edit(conn, %{"id" => id}) do
-    project = Repo.get(Project, id)
-    changeset = Project.changeset(project)
-    render conn, :edit, project: project, changeset: changeset
+    changeset = Project.changeset(conn.assigns[:project])
+    render conn, :edit, changeset: changeset
   end
 
   def update(conn, %{"id" => id, "project" => project_params}) do
-    project = Repo.get(Project, id)
-    changeset = Project.changeset(project, project_params)
+    changeset = Project.changeset(conn.assigns[:project], project_params)
     if changeset.valid? do
       Repo.update(changeset)
-      conn
-      |> put_flash(:info, "Project updated succesfully.")
-      |> redirect to: project_path(conn, :show, project)
+      conn  |> put_flash(:info, "Project updated succesfully.")
+            |> redirect to: project_path(conn, :show, conn.assigns[:project])
     else
-      render conn, :edit, project: project, changeset: changeset
+      render conn, :edit, changeset: changeset
     end
   end
 
-  def delete(conn, %{"id" => id}) do
-    project = Repo.get(Project, id)
-    Repo.delete(project)
-    conn
-    |> put_flash(:info, "Project deleted successfully")
-    |> redirect to: project_path(conn, :index)
+  def delete(conn, _) do
+    Repo.delete(conn.assigns[:project])
+    conn  |> put_flash(:info, "Project deleted successfully")
+          |> redirect to: project_path(conn, :index)
+  end
+
+  defp load_project(conn, _) do
+    case Repo.get(Project, conn.params["id"]) do
+      nil ->
+        conn  |> put_flash(:error, "The project you were looking for could not be found.")
+              |> redirect(to: project_path(conn, :index))   |> halt
+      project ->
+        assign(conn, :project, project)
+    end
   end
 end
