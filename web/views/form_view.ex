6801a10053357c116edff17776c33c9b21265cc4
Rebecca Skinner <traybaby@gmail.com>
2015-06-13 08:52:14 +0800

    Display better form errors

--- web/views/form_view.ex
+++ web/views/form_view.ex
@@ -2,12 +2,16 @@ defmodule Ticketee.FormView do
   import Phoenix.HTML.Tag
 
   def error(f, field, opts \\ []) do
-    if f.errors[field] do
+    if has_error(f, field) do
       content_tag :div, opts do
         content_tag :p, class: "help-block" do
           f.errors[field]
         end
       end
     end
+  end
+
+  def has_error(f, field) do
+    f.errors[field]
   end
 end
